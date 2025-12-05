8a20fbaa01376d679b1abcb9099abc99c187df43
MichaelFetene123 <michaelfetene0202@gmail.com>
2025-12-05 16:14:04 +0300

    Add Laravel Sanctum for API authentication and update dependencies
    - Integrated Laravel Sanctum into the User model for API token management.
    - Updated composer.json to include Sanctum as a dependency.
    - Added axios for API requests in the React frontend and updated package.json and package-lock.json accordingly.
    - Enhanced Signup component to handle user registration with error handling and state management.
    - Updated .gitignore files to exclude additional files.

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function Login() {
   const onSubmit = (e) => {
     e.preventDefault();
+    
   };
   return (
     <div className="login-signup-form animated fadeInDown">
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,19 +1,63 @@
-import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Signup() {
+const nameRef = useRef();
+const emailRef = useRef();
+const passwordRef = useRef();
+const passwordComfirmationRef = useRef();
+const [errors, setErrors] = useState();
+const navigate = useNavigate();
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+const {setToken,setUser} = useStateContext();
+const onSubmit = async (e) => {
+  e.preventDefault();
+
+  const payload = {
+    name: nameRef.current.value,
+    email: emailRef.current.value,
+    password: passwordRef.current.value,
+    password_confirmation: passwordComfirmationRef.current.value,
+  };
+
+  setErrors(null);
+
+  try {
+    const response = await axiosClient.post("/signup", payload);
+    console.log(response);
+    console.log(response.data);
+    console.log(response.data.user);
+    console.log(response.data.token);
+    
+    setUser(response.data.user);
+    setToken(response.data.token);
+  
+  } catch (err) {
+    const response = err.response;
+    if (response && response.status === 422) {
+      console.log(response.data.errors);
+      setErrors(response.data.errors);
+    }
   }
+};
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Sign Up for an account</h1>
-          <input placeholder="Full Name" type="text" />
-          <input placeholder="Email" type="email" />
-          <input placeholder="Password" type="password" />
-          <input placeholder="Confirm Password" type="password" />
+
+          {errors && (<div className="alert">
+            {Object.keys(errors).map(key => (
+              <p key={key}>{errors[key][0]}</p>
+            ))}
+          </div>)}
+
+          <input ref={nameRef} placeholder="Full Name" type="text" />
+          <input ref={emailRef} placeholder="Email" type="email" />
+          <input ref={passwordRef} placeholder="Password" type="password" />
+          <input ref={passwordComfirmationRef} placeholder="Confirm Password" type="password" />
           <button className="btn btn-block" type="submit">
             Sign Up
           </button>
